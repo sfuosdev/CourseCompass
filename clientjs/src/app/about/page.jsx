a8531f5517e852757fc8934a6ef54c29005a7013
Dreamz2 <Edwei395@gmail.com>
2023-11-28 21:20:19 -0800

    About page finished. Description, Picture, and Links can be added as we go

---
+++ clientjs/src/app/about/page.jsx
@@ -0,0 +1,72 @@
+import Link from "next/link";
+
+const founders = [
+    {
+        name: "Sallin",
+        links: "/",
+        role: "Tech Lead",
+        image: "/profile-picture.jpg"
+    },
+    {
+        name: "Kiran",
+        links: "/",
+        role: "Designer",
+        image: "/profile-picture.jpg"
+    },
+    {
+        name: "Joao",
+        links: "/",
+        role: "Tech Lead",
+        image: "/profile-picture.jpg"
+    },
+    {
+        name: "Edison",
+        links: "/",
+        role: "Tech Lead",
+        image: "/profile-picture.jpg"
+    },
+    {
+        name: "Samuel",
+        links: "/",
+        role: "Tech Lead",
+        image: "/profile-picture.jpg"
+    }
+]
+let final;
+
+export default function About() {
+
+    return (
+        <div className=" py-6 flex flex-col items-center">
+            <div className="w-1/4 p-6">
+                <p className="py-3 text-3xl font-bold">Welcome to Course Compass!</p>
+                <p className="text-lg font-normal">
+                    Course Compass is currently a course planning website for SFU students.
+                    Our goal is to offer a comprehesive guide on academic planning in your degree.
+                </p>
+            </div>
+
+            <p className=" text-2xl font-semibold">Course Compass Founders</p>
+            {founders.map(profile => (
+        <Profile name={profile.name} role={profile.role} link={profile.links} image={profile.image} />
+    ))}
+        </div>
+    );
+}
+
+function Profile({name, role, link, image}) {
+    return (
+        <div className={`p-12 grid grid-cols-2 place-items-center`}>
+            <div className="px-0">
+                <img src={image} alt={"Picture of "+ name} className="h-52 w-52 rounded-full"/>
+            </div>
+            <div className="py-2 w-96">
+                <p className="text-xl font-bold">{name}</p>
+                <p className="py-2 text-lg font-medium">{role}</p>
+                {/* Change the if multiple links to map function*/}
+                <Link href={link} className="text-lg font-normal">Linkedin</Link>
+                <p className="text-lg font-normal"></p>
+            </div>
+        </div>
+    );
+}
