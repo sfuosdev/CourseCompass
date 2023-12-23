fe26010ca43dc5b41213bc0de558d66815b10ec3
Dreamz2 <Edwei395@gmail.com>
2023-12-23 14:02:26 -0800

    Review card and Professor cards made, but need to be fixed and add tailwind css to both

---
+++ clientjs/src/components/ProfessorCard.jsx
@@ -0,0 +1,226 @@
+import Link from "next/link";
+import { useState } from "react";
+
+// Fix tailwind
+{/* <div className={`grid grid-cols-2`}> 
+<ProfessorCard course={courses[0]} />
+<ProfessorCard course={courses[1]}/>
+</div> */}
+const ProfessorCard = ({course}) => {
+    const [view, setView] = useState('');
+    const [flip, setFlip] = useState(false);
+
+    if(course.length > 1)
+        console.log(1);
+
+    return (
+        
+        <div className="p-2 w-80 h-[360px] flex">
+            <div className="flex flex-col round rounded-2xl w-[270px] h-[360px] bg-[#F3F4FA] pl-[18px] pr-[18px] pt-[25px] hover:bg-[#AAC2FD] hover:shadow-lg focus-within:bg-[#AAC2FD]">
+                <div className="text-[30px] font-medium">{course[0].professor === 'TBA'? "TBA" : course[0].professor}</div>
+                    <div className="inline-block font-medium text-[15px] h-[40%]">Class Schedule:</div>
+                        <div className="flex justify-between inline-block text-[15px] font-bold">
+                            <span>Lecture Section: {course[0].sections[0]}</span>
+                            <span>Dates</span>
+                            <span>{course.sectionCode == 'LAB'? 'Lab Section': 'Tutorial Sections'}</span>
+                        </div>
+                        <hr className="h-px my-[5px] bg-black"></hr>
+                    <div className="flex justify-between inline-block text-[15px] h-[25%]">
+                    <span>Professor:</span>
+                    <div className="flex flex-col text-right">
+                        <span>Professor</span>
+                    </div>
+                </div>
+                <Link
+                href={`/courses/2024/spring/cmpt/225`}
+                className="underline underline-offset-4 text-[10px] text-[#4570E6] text-right pb-[5px] hover:text-[#F7C750]"
+                >
+                View complete professor ratings
+                </Link>
+            </div>
+        </div>
+    );
+}
+export default ProfessorCard;
+
+// {
+//     "text": "225",
+//     "value": "225",
+//     "title": "Data Structures and Programming",
+//     "instructor": [
+//       "Anne Lavergne",
+//       "Toby Donaldson"
+//     ],
+//     "prerequisites": "(MACM 101 and (CMPT 125, CMPT 129 or CMPT 135)) or (ENSC 251 and ENSC 252), all with a minimum grade of C-.",
+//     "corequisites": "",
+//     "sections": [
+//       {
+//         "text": "D100",
+//         "value": "d100",
+//         "title": "Data Structures and Programming",
+//         "classType": "e",
+//         "sectionCode": "LEC",
+//         "associatedClass": "1"
+//       },
+//       {
+//         "text": "D101",
+//         "value": "d101",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "1"
+//       },
+//       {
+//         "text": "D102",
+//         "value": "d102",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "1"
+//       },
+//       {
+//         "text": "D103",
+//         "value": "d103",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "1"
+//       },
+//       {
+//         "text": "D104",
+//         "value": "d104",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "1"
+//       },
+//       {
+//         "text": "D105",
+//         "value": "d105",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "1"
+//       },
+//       {
+//         "text": "D106",
+//         "value": "d106",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "1"
+//       },
+//       {
+//         "text": "D107",
+//         "value": "d107",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "1"
+//       },
+//       {
+//         "text": "D108",
+//         "value": "d108",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "1"
+//       },
+//       {
+//         "text": "D200",
+//         "value": "d200",
+//         "title": "Data Structures and Programming",
+//         "classType": "e",
+//         "sectionCode": "LEC",
+//         "associatedClass": "2"
+//       },
+//       {
+//         "text": "D201",
+//         "value": "d201",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "2"
+//       },
+//       {
+//         "text": "D202",
+//         "value": "d202",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "2"
+//       },
+//       {
+//         "text": "D203",
+//         "value": "d203",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "2"
+//       },
+//       {
+//         "text": "D204",
+//         "value": "d204",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "2"
+//       },
+//       {
+//         "text": "D205",
+//         "value": "d205",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "2"
+//       },
+//       {
+//         "text": "D206",
+//         "value": "d206",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "2"
+//       },
+//       {
+//         "text": "D207",
+//         "value": "d207",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "2"
+//       },
+//       {
+//         "text": "D208",
+//         "value": "d208",
+//         "title": "Data Structures and Programming",
+//         "classType": "n",
+//         "sectionCode": "LAB",
+//         "associatedClass": "2"
+//       }
+//     ],
+//     "description": "Introduction to a variety of practical and important data structures and methods for implementation and for experimental and analytical evaluation. Topics include: stacks, queues and lists; search trees; hash tables and algorithms; efficient sorting; object-oriented programming; time and space efficiency analysis; and experimental evaluation. ",
+//     "deliveryMethod": "In Person",
+//     "courseSchedule": [
+//       {
+//         "endDate": "Fri Oct 06 00:00:00 PDT 2023",
+//         "campus": "Surrey",
+//         "days": "Tu",
+//         "sectionCode": "LAB",
+//         "startTime": "11:30",
+//         "isExam": false,
+//         "endTime": "12:20",
+//         "startDate": "Wed Sep 06 00:00:00 PDT 2023"
+//       },
+//       {
+//         "endDate": "Tue Dec 05 00:00:00 PST 2023",
+//         "campus": "Surrey",
+//         "days": "Tu",
+//         "sectionCode": "LAB",
+//         "startTime": "11:30",
+//         "isExam": false,
+//         "endTime": "12:20",
+//         "startDate": "Wed Oct 11 00:00:00 PDT 2023"
+//       }
+//     ]
+//   }
---
+++ clientjs/src/components/ReviewList.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+const upArrows = ["⇧", "⬆"];
+const downArrows = ["⇩", "⬇"];
+
+function setUserRating() {
+
+}
+
+
+// userEX is user
+const userEX = {
+    ID: String,
+    like: Boolean,
+    dislike: Boolean
+};
+
+// Assuming "user" and the "review" have the same ID numbers
+// Going to assume that the "review" and "user" are parsed
+// "user" will contain the above or more information
+export default function Review({ review, user }) {
+    const [like, setLike] = useState(false);
+    const [dislike, setDisLike] = useState(false);
+    const [likeCounter, setlikesCounter] = useState(review.likes);
+    const [dislikeCounter, setdislikesCounter] = useState(review.dislikes);
+    const votes = {like: review.likes, dislike: review.dislikes}
+
+    function handleUpVote() {
+        setLike(!like);
+        if(!like) {
+            setDisLike(like);
+            setlikesCounter(likeCounter + 1);
+        }
+        else
+            setlikesCounter(likeCounter - 1);
+            // fix counter later
+        // Both need updates for the server
+    }
+
+    function handleDownVote() {
+        setDisLike(!dislike);
+        if(!dislike) {
+            setLike(dislike);
+            setdislikesCounter(dislikeCounter + 1);
+        }
+        else
+            setdislikesCounter(dislikeCounter - 1);
+    }
+
+    // Change the width
+    return (
+        <div className="relative md:p-4 mx-5 my-2 md:w-3/4 flex flex-row border-b-4 border-black hover:bg-gray">
+            <div className="flex flex-col justify-around  md:justify-between w-1/2">
+                <div className="flex flex-col items-stretch text-[10px] md:text-[20px]">
+                    {`${review.day}/${review.month}/${review.year}`}
+                    <div>
+                        {review.name}
+                    </div>
+                    <img src={review.image}
+                        alt={`Picture of ${review.name}`}
+                        className="self-center h-8 w-8 md:h-16 md:w-16 rounded-full"></img>
+                </div>
+                <div className="flex px-4">
+                    <button className="px-2 text-[12px] md:text-[20px]" onClick={handleUpVote}>
+                        {`${like == true ? upArrows[1] : upArrows[0]}`} <span className="text-[8px] md:text-[12px]">{likeCounter}</span>
+                    </button>
+                    {/* Remove space next to span to put 1000 on the right of arrow */}
+                    <button className="px-2 text-[12px] md:text-[20px]" onClick={handleDownVote}>
+                        {dislike == true ? downArrows[1] : downArrows[0]} <span className="text-[8px] md:text-[12px]">{dislikeCounter}</span>
+                    </button>
+                </div>
+            </div>
+            <div className="p-3 md:p-0 flex flex-col w-auto justify-between">
+                <div className="lg:py-3 text-[10px] md:text-[18px]">{review.comment}</div>
+                <div className="grid grid-cols-3 text-[10px] md:text-[18px]">
+                    <div className="font-semibold">Professor: <span className="font-normal">{review.professor}</span></div>
+                    <div className="font-semibold">Clarity: <span className="font-normal">{`${review.clarity}/5`}</span></div>
+                    <div className="font-semibold">Engagement: <span className="font-normal">{`${review.engagement}/5`}</span></div>
+                </div>
+            </div>
+        </div>
+    );
+}
