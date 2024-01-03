cd4ad36ea0672899ba4d46a574bcb88089d72355
samuel-antunes <samuel_antunes1@hotmail.com>
2024-01-02 23:57:01 -0800

    Changes to profile page, including selected views and responsiviness

--- clientjs/src/app/profile/page.jsx
+++ clientjs/src/app/profile/page.jsx
@@ -1,131 +1,172 @@
 "use client";
 
-import ProfileHero from "@/components/ProfileHero"
+import ProfileHero from "@/components/ProfileHero";
 import ProfileCompletionList from "@/components/ProfileCompletionList";
 import FavouriteCoursesList from "@/components/FavouriteCoursesList";
-import { useState } from "react"
+import { useState } from "react";
+import { departments } from "@/components/LoginSignupModal";
 
 const userInfo = {
-    fullname: "Peter Chan",
-    email: "peterchan214@gmail.com",
-    credits: 40,
-    year: "2",
-    major: "Computer Science",
-    minor: "",
-    schedule: "",
-    coursereviews: [],
-    professorreviews: [],
-}
+  fullname: "Peter Chan",
+  email: "peterchan214@gmail.com",
+  credits: 40,
+  year: "2",
+  major: "Computer Science",
+  minor: "",
+  schedule: "",
+  coursereviews: [],
+  professorreviews: [],
+};
 
 const year = () => {
-    switch (userInfo.year) {
-        case 1: return "1 "
-    }
-}
+  switch (userInfo.year) {
+    case 1:
+      return "1 ";
+  }
+};
 
 function UploadScheduleView() {
-    return (
-        <div className="bg-[#4D4D4D] h-[50%] w-auto">
-            <div className="px-16 py-16">
-                <div className="py-3 font-medium text-[25px]">Import an option to get started</div>
-                <div className="pb-9 font-normal text-[18px]">Input your previous course history</div>
-                <div className="bg-[#2D54A6] flex">
-                    <div className="">Change to input</div>
-                    <div className="">
-                        <div className="">
-
-                        </div>
-                        <div className="">
-
-                        </div>
-                    </div>
-                </div>
-                <div className="bg-gray">
-
-                </div>
-            </div>
+  return (
+    <div className="h-[50%] w-auto">
+      <div className="p-[20px]">
+        <div className="py-3 font-medium text-[30px]">
+          Import your current schedule
         </div>
-    );
+        <div className="pb-9 font-normal text-[18px]">
+          Import your current schedule by clicking here
+        </div>
+      </div>
+    </div>
+  );
 }
 function ImportCoursesView() {
-    return (
-        <div className="bg-[#AAC2FD] h-[50%] w-auto">
-            <div className="px-16 py-16">
-                <div className="py-3 font-medium text-[25px]">Import an option to get started</div>
-                <div className="pb-9 font-normal text-[18px]">Input your previous course history</div>
-                <div className="bg-[#EAF0FF] flex">
-                    <div className="">Change to input</div>
-                    <div className="">
-                        <div className="">
-
-                        </div>
-                        <div className="">
-
-                        </div>
-                    </div>
-                </div>
-                <div className="bg-gray">
-
-                </div>
-            </div>
+  return (
+    <div className="h-[50%] w-auto">
+      <div className="p-[20px]">
+        <div className="py-3 font-medium text-[30px]">
+          Import course history
         </div>
-    );
+        <div className="pb-9 font-normal text-[18px]">
+          Import your course history to leave reviews by either uploading your
+          transcript or adding the courses manually{" "}
+          {
+            <a
+              href="/transcript"
+              className="underline underline-offset-3 text-primary-blue"
+            >
+              here
+            </a>
+          }
+        </div>
+      </div>
+    </div>
+  );
 }
 function MajorInfoView() {
-    return (
-        <div className="bg-gray-200 h-[50%] w-auto">
-            <div className="px-16 py-16">
-                <div className="py-3 font-semibold text-[25px]">Add your degree information</div>
-                <div className="pb-9 font-normal text-[16px]">Please provide additional information on your (intended) program, major(s), minor(s), specialization(s), and concentration(s).</div>
-                <div className="bg-gray-400 flex h-[100px]">
-                    <div className="">
-                        <div className="">
+  const [formData, setFormData] = useState({
+    majors: [],
+    minors: [],
+  });
 
-                        </div>
-                        <div className="">
+  const handleInputChange = (e) => {
+    const options = e.target.options;
+    let value = [];
+    for (let i = 0, l = options.length; i < l; i++) {
+      if (options[i].selected) {
+        if (options[i].value === "none") {
+          // If 'None' is selected, reset the selection
+          value = [];
+          break;
+        } else {
+          value.push(options[i].value);
+        }
+      }
+    }
+    setFormData({ ...formData, [e.target.name]: value });
+  };
 
-                        </div>
-                    </div>
-                </div>
-                <div className="bg-gray">
-
-                </div>
-            </div>
+  return (
+    <div className="h-[50%] w-auto">
+      <div className="p-[20px]">
+        <div className="py-3 font-semibold text-[30px]">
+          Add your degree information
         </div>
-    );
+        <div className="pb-9 font-normal text-[16px]">
+          Please provide additional information on your (intended) program,
+          major(s), minor(s), specialization(s), and concentration(s).
+        </div>
+        <form>
+          <select
+            multiple
+            name="majors"
+            value={formData.majors}
+            onChange={handleInputChange}
+            className="block w-full p-2 mb-2"
+          >
+            <option value="none">None</option>
+            {departments.map((dept) => (
+              <option key={dept.value} value={dept.value}>
+                {dept.name}
+              </option>
+            ))}
+          </select>
+          <label htmlFor="minors">Minors:</label>
+          <select
+            multiple
+            name="minors"
+            value={formData.minors}
+            onChange={handleInputChange}
+            className="block w-full p-2 mb-2"
+          >
+            <option value="none">None</option>
+            {departments.map((dept) => (
+              <option key={dept.value} value={dept.value}>
+                {dept.name}
+              </option>
+            ))}
+          </select>
+          <button
+            type="submit"
+            className="block w-full bg-primary-blue text-white p-2 rounded mb-2"
+          >
+            Update information
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-
 function SwitchComponent({ viewSelector }) {
-    switch(viewSelector) {
-        case 1:
-            return <UploadScheduleView />;
-        case 2:
-            return <ImportCoursesView />;
-        case 3:
-            return <MajorInfoView />;
-        default:
-            return (userInfo.schedule === ""? "":<UploadScheduleView />)
-    } 
+  switch (viewSelector) {
+    case 1:
+      return <UploadScheduleView />;
+    case 2:
+      return <ImportCoursesView />;
+    case 3:
+      return <MajorInfoView />;
+    default:
+      return userInfo.schedule === "" ? "" : <UploadScheduleView />;
+  }
 }
 
 export default function Profile() {
-    const [viewSelector, setViewSelector] = useState(1);
+  const [viewSelector, setViewSelector] = useState(1);
 
-    return (
-        <>
-            <ProfileHero fullname={userInfo.fullname} email={userInfo.email} />
+  return (
+    <div>
+      <ProfileHero fullname={userInfo.fullname} email={userInfo.email} />
 
-            <div className="grid grid-cols-4 md:h-screen">
-                <div className="col-span-3">
-                    <SwitchComponent viewSelector={viewSelector} />
-                    {/* <MajorInfoView /> */}
-                </div>
-                <div className="flex flex-col">
-                    <ProfileCompletionList setViewSelector={setViewSelector}/>
-                    <FavouriteCoursesList setViewSelector={setViewSelector}/>
-                </div>
-            </div>
-        </>
-    )
+      <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col w-screen md:w-[30%]">
+          <ProfileCompletionList setViewSelector={setViewSelector} />
+          <FavouriteCoursesList setViewSelector={setViewSelector} />
+        </div>
+        <div className="w-screen md:w-[70%]">
+          <SwitchComponent viewSelector={viewSelector} />
+          {/* <MajorInfoView /> */}
+        </div>
+      </div>
+    </div>
+  );
 }
--- clientjs/src/components/FavouriteCoursesList.jsx
+++ clientjs/src/components/FavouriteCoursesList.jsx
@@ -1,20 +1,20 @@
 // Grab user data
 
 const FavouriteCoursesList = () => {
-    //Grab user data for Favourite course
-    // or grab reviews??
-    
-    const courseList = "";
+  //Grab user data for Favourite course
+  // or grab reviews??
 
-    return (
-        <div className="flex flex-col pt-3 pb-5 pl-1 pr-2 m-2 border shadow-md rounded-lg lg:w-6/7 xl:w-4/5">
-            <div className="py-3 font-medium text-[22px]">Favourites</div>
-            <hr></hr>
-            <div>
-                {courseList === ""? "No favourite courses found": "coursList"}
-            </div>
-        </div>
-    );
-}
+  const courseList = "";
+
+  return (
+    <div className="flex flex-col pt-3 pb-5 pl-1 pr-2 m-2 border shadow-md rounded-lg">
+      <div className="py-3 font-medium text-[22px]">Favourites</div>
+      <hr></hr>
+      <div>
+        {courseList === "" ? "No favourite courses found" : "coursList"}
+      </div>
+    </div>
+  );
+};
 
 export default FavouriteCoursesList;
