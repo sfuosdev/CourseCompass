28a35eabb0428d79d8d379976fcc70857b94b4f8
Sallin142 <Sallin.koutev@gmail.com>
2024-03-31 18:16:24 -0700

    added completed courses functionality, add

--- clientjs/src/pages/api/user/addFavoriteCourse.js
+++ clientjs/src/pages/api/user/addFavoriteCourse.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     await dbConnect();
 
     const { userId, courseCode } = req.body;
-
+    console.log("ADDFAVORITECOURSE", req.body)
     try {
         const course = await Course.findOne({ courseCode });
         if (!course) return res.status(404).json({ message: 'Course not found' });
--- clientjs/src/pages/api/user/addTakenCourse.js
+++ clientjs/src/pages/api/user/addTakenCourse.js
@@ -1,21 +1,34 @@
-// api/user/addTakenCourse.js
 import User from '../../../models/User';
 import Course from '../../../models/Course';
-import dbConnect from '../../../app/utils/dbConnect'
+import dbConnect from '../../../app/utils/dbConnect';
 
 export default async function handler(req, res) {
     await dbConnect();
 
-    const { userId, courseCode, grade, semester, year } = req.body;
+    // Format the courseCode to remove spaces
+    const { userId, courseCode, semester, year } = req.body;
+    const formattedCourseCode = courseCode.replace(/\s+/g, '');
+
+    console.log("userId:", userId, "courseCode:", formattedCourseCode, "semester:", semester, "year:", year);
 
     try {
-        const course = await Course.findOne({ courseCode });
-        if (!course) return res.status(404).json({ message: 'Course not found' });
+        // Use the formatted courseCode for querying
+        const course = await Course.findOne({ courseCode: formattedCourseCode });
+        if (!course) {
+            return res.status(404).json({ message: 'Course not found' });
+        }
 
-        const completedCourse = { course: course._id, grade, semester, year };
-        await User.findByIdAndUpdate(userId, { $push: { completedCourses: completedCourse } });
+        const completedCourse = {
+            course: course._id,
+            semesterCompleted: semester,
+            yearCompleted: year,
+        };
+
+        await User.findByIdAndUpdate(userId, { $addToSet: { completedCourses: completedCourse } });
+
         res.status(200).json({ message: 'Taken course added successfully' });
     } catch (error) {
+        console.error(error);
         res.status(500).json({ error: error.message });
     }
-}
+};
